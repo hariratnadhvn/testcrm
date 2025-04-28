7a313f74facf80f7c473cf75ebcfbdbad5ff6ace
hariratnadhvn <hariratnadhvn@hotmail.com>
2025-04-28 14:00:11 +0530

    Deploy build to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/testcrm/',  // Add this line to specify the base path for GitHub Pages
+  
 });
